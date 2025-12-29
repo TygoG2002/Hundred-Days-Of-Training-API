caefae772efdaf7bd83ef222e32edc2e38f68208
Tygo Geervliet <tygo@tygodev.com>
2025-12-29 14:55:23 +0100

    git commit -m "Fix CORS for production"

--- HundredDays.Api/Program.cs
+++ HundredDays.Api/Program.cs
@@ -5,26 +5,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("BlazorDev", policy =>
-    {
-        policy
-            .WithOrigins("https://localhost:7288")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
-    });
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod());
 });
 
 builder.Services.AddControllers();
 
-
 builder.Services.AddApplication();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddInfrastructure(connectionString!);
 
 var app = builder.Build();
-app.UseCors("BlazorDev");
 
-app.UseAuthorization();
+app.UseCors("AllowAll");
+
 app.MapControllers();
 
 app.Run();

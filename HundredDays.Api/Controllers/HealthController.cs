d7094fdf3fe453ec871cd374d7d6d527457d628f
Tygo Geervliet <tygo@tygodev.com>
2025-12-29 19:37:38 +0100

    Add HealthController with /health GET endpoint
    Introduced HealthController with [ApiController] and [Route("health")]. Added a GET action that returns "Healthy" for health checks.

---
+++ HundredDays.Api/Controllers/HealthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Health() => Ok("Healthy");
+}

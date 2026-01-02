78562c7aed377078bac9fdf481cd20b5c2f3442f
TygoG2002 <Tygo@tygodev.com>
2026-01-02 21:56:50 +0100

    prevent transient MariaDB failures by pinning server version and enabling retries

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -10,14 +10,25 @@ namespace Infrastructure
             this IServiceCollection services,
             string connectionString)
         {
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseMySql(
-                    connectionString,
-                    ServerVersion.AutoDetect(connectionString)
-                )
+            services.AddDbContext<AppDbContext>(
+                options =>
+                    options.UseMySql(
+                        connectionString,
+                        new MySqlServerVersion(new Version(11, 8, 3)), 
+                        mySqlOptions =>
+                        {
+                            mySqlOptions.EnableRetryOnFailure(
+                                maxRetryCount: 5,
+                                maxRetryDelay: TimeSpan.FromSeconds(5),
+                                errorNumbersToAdd: null
+                            );
+                        }
+                    ),
+                ServiceLifetime.Scoped 
             );
 
             services.AddScoped<IWorkoutRepository, WorkoutRepository>();
+
             return services;
         }
     }

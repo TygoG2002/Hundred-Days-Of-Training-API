9ec3695d9217eeaa365c0e9f9749bbfaa72fbb71
Tygo Geervliet <tygo@tygodev.com>
2026-01-11 15:10:44 +0100

    Optimize plans overview by removing N+1 day queries

--- Application/Plans/GetPlansOverview/GetPlansOverviewHandler.cs
+++ Application/Plans/GetPlansOverview/GetPlansOverviewHandler.cs
@@ -1,46 +1,22 @@
-﻿using Application.Days.Interfaces;
+﻿using Application.Plans.GetPlansOverview;
 using Application.Plans.GetPlansWithProgress;
 using Application.Plans.Interfaces;
 using MediatR;
 
-namespace Application.Plans.GetPlansOverview;
-
-public class GetPlansOverviewHandler : IRequestHandler<GetPlansOverviewQuery, List<PlanOverviewDto>>
+public class GetPlansOverviewHandler
+    : IRequestHandler<GetPlansOverviewQuery, List<PlanOverviewDto>>
 {
     private readonly IPlanQueryRepository _plans;
-    private readonly IDayQueryRepository _days;
 
-    public GetPlansOverviewHandler(IPlanQueryRepository plans, IDayQueryRepository days, IDayQueryRepository progress)
+    public GetPlansOverviewHandler(IPlanQueryRepository plans)
     {
         _plans = plans;
-        _days = days;
     }
 
-    public async Task<List<PlanOverviewDto>> Handle(GetPlansOverviewQuery request, CancellationToken cancellationToken)
+    public async Task<List<PlanOverviewDto>> Handle(
+        GetPlansOverviewQuery request,
+        CancellationToken cancellationToken)
     {
-        var plans = await _plans.GetAllAsync();
-        var result = new List<PlanOverviewDto>();
-
-        foreach (var plan in plans)
-        {
-            var days = await _days.GetDaysAsync(plan.Id);
-
-
-            int completed = 0; 
-            foreach (var day in days)
-            {
-                if (day.IsCompleted)
-                    completed++;
-            }
-            
-            result.Add(new PlanOverviewDto(
-                plan.Id,
-                plan.Name,
-                completed,
-                days.Count
-            ));
-        }
-
-        return result;
+        return await _plans.GetPlansOverviewAsync();
     }
 }
--- Application/interfaces/IPlanQueryRepository.cs
+++ Application/interfaces/IPlanQueryRepository.cs
@@ -1,8 +1,12 @@
-﻿using HundredDays.Domain.Entities;
+﻿using Application.Plans.GetPlansWithProgress;
+using HundredDays.Domain.Entities;
 
 namespace Application.Plans.Interfaces;
 
 public interface IPlanQueryRepository
 {
     Task<List<WorkoutPlan>> GetAllAsync();
+
+    Task<List<PlanOverviewDto>> GetPlansOverviewAsync();
+
 }

6baf4b6287b3ca92cc574e9445c25d68060bb660
Tygo Geervliet <tygo@tygodev.com>
2026-01-11 16:59:14 +0100

    added controller

---
+++ HundredDays.Api/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/dashboard")]
+public class DashboardController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DashboardController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("today")]
+    public async Task<IActionResult> GetTodayWorkouts()
+    {
+       
+        return Ok();
+    }
+}

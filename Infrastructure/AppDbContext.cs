3ae3905ecf7420a8207ce0e50f5a658cd24b320b
TygoG2002 <Tygo@tygodev.com>
2026-01-13 12:14:18 +0100

    run migrations EF

--- Infrastructure/AppDbContext.cs
+++ Infrastructure/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using HundredDays.Domain.Entities;
+﻿using Domain.Entities.Template;
+using HundredDays.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 public class AppDbContext : DbContext
@@ -7,6 +8,10 @@ public class AppDbContext : DbContext
     public DbSet<WorkoutPlan> WorkoutPlans => Set<WorkoutPlan>();
     public DbSet<WorkoutDay> WorkoutDays => Set<WorkoutDay>();
     public DbSet<WorkoutSet> WorkoutSets => Set<WorkoutSet>();
+
+    public DbSet<WorkoutTemplate> WorkoutTemplate => Set<WorkoutTemplate>();
+
+    public DbSet<TemplateExercise> TemplateExercise => Set<TemplateExercise>();
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
     : base(options) { }
---
+++ Infrastructure/Migrations/20260113111210_add-template.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkoutTemplate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    EstimatedDurationMinutes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkoutTemplate", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TemplateExercise",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Sets = table.Column<int>(type: "int", nullable: false),
+                    Reps = table.Column<int>(type: "int", nullable: false),
+                    RestSeconds = table.Column<int>(type: "int", nullable: false),
+                    WorkoutTemplateId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TemplateExercise", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TemplateExercise_WorkoutTemplate_WorkoutTemplateId",
+                        column: x => x.WorkoutTemplateId,
+                        principalTable: "WorkoutTemplate",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TemplateExercise_WorkoutTemplateId",
+                table: "TemplateExercise",
+                column: "WorkoutTemplateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TemplateExercise");
+
+            migrationBuilder.DropTable(
+                name: "WorkoutTemplate");
+        }
+    }
+}

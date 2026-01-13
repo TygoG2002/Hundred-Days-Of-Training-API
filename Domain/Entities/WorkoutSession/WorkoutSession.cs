f01f1dd701f6881f4073c50f624e4514edee7371
TygoG2002 <Tygo@tygodev.com>
2026-01-13 16:45:02 +0100

    WorkoutSession entity

---
+++ Domain/Entities/WorkoutSession/WorkoutSession.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities.Template;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.WorkoutSession
+{
+    public class WorkoutSession
+    {
+        public int Id { get; private set; }
+        public int WorkoutTemplateId { get; private set; }
+
+        public WorkoutTemplate WorkoutTemplate { get; private set; } = null!;
+
+
+        public DateTime StartedAt { get; private set; }
+
+        public DateTime? FinishedAt { get; private set; }
+
+        private WorkoutSession() 
+        {
+        }
+
+        public WorkoutSession(int workoutTemplateId)
+        {
+            WorkoutTemplateId = workoutTemplateId;
+            StartedAt = DateTime.UtcNow;
+        }
+
+        public void Finish()
+        {
+            FinishedAt = DateTime.UtcNow;
+        }
+
+    }
+}

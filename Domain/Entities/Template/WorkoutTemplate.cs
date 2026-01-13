4841d00bdab5237b87b08092ae115884092fffc0
TygoG2002 <Tygo@tygodev.com>
2026-01-13 11:27:30 +0100

    seperate folders. per feature

---
+++ Domain/Entities/Template/WorkoutTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Template
+{
+    public class WorkoutTemplate
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
